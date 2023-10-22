39045a023ad16d8d264929edeeab9def73b17b3e
cassiolpaixao90 <cassiopaixaoti@gmail.com>
2023-10-22 19:07:01 -0300

    feat: add mongo connection

--- pkg/mongo/connection_provider.go
+++ pkg/mongo/connection_provider.go
@@ -1,1 +1,37 @@
 package mongo
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"sync"
+)
+
+var (
+	mongoClient *mongo.Client
+	once        sync.Once
+)
+
+type MongoConfig struct {
+	URI    string
+	Logger interface{}
+}
+
+func NewMongoConnection(cfg *MongoConfig) *MongoConfig {
+	return &MongoConfig{URI: cfg.URI}
+}
+
+func (m *MongoConfig) GetMongoClient() *mongo.Client {
+	once.Do(func() {
+		clientOptions := options.Client().ApplyURI(m.URI)
+
+		client, err := mongo.Connect(context.Background(), clientOptions)
+		if err != nil {
+			return
+		}
+
+		mongoClient = client
+	})
+
+	return mongoClient
+}
