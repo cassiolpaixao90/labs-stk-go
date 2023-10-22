193a944e700aa70c789e9db04a11e6da602f39a9
cassiolpaixao90 <cassiopaixaoti@gmail.com>
2023-10-22 14:08:49 -0300

    feat: initial commit

---
+++ internal/core/ports/in/book_service_port.go
@@ -0,0 +1,16 @@
+package in
+
+import "labs-stk-go-hexagonal/internal/core/domain"
+
+type ICreate interface {
+	Create(user *domain.BookDomain) error
+}
+
+type IGetByID interface {
+	GetByID(id int) (*domain.BookDomain, error)
+}
+
+type IBookServicePort interface {
+	ICreate
+	IGetByID
+}
---
+++ internal/core/ports/out/book_repository_port.go
@@ -0,0 +1,4 @@
+package out
+
+type IBookRepository interface {
+}
